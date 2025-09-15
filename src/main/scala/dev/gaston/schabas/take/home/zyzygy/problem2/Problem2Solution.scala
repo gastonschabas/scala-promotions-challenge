9614f6e2b6a0048278418ddfdb09f8f3b865df41
Gastón Schabas <gastonschabas@gmail.com>
2025-09-15 12:19:17 -0300

    feature - solve problem 2 (#4)
    * solve problem 2
    * update README detailing solution problem 2

--- src/main/scala/dev/gaston/schabas/take/home/zyzygy/problem2/Problem2Solution.scala
+++ src/main/scala/dev/gaston/schabas/take/home/zyzygy/problem2/Problem2Solution.scala
@@ -3,14 +3,92 @@ package dev.gaston.schabas.take.home.zyzygy.problem2
 object Problem2Solution:
 
   case class Promotion(code: String, notCombinableWith: Seq[String])
-  case class PromotionCombo(promotionCodes: Seq[String])
+  case class PromotionCombo(promotionCodes: Seq[String]):
+    def withSortedCodes: PromotionCombo = copy(promotionCodes = promotionCodes.sorted)
 
   /**
-   * Implement a function to find all PromotionCombos with maximum number of combinable promotions in each.
+   * @param allPromotions all available promotions
+   * @return all PromotionCombos with maximum number of combinable promotions in each.
    */
-  def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] = ???
+  def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] =
+    val allValidCombos = generateAllValidCombinations(allPromotions)
+    findMaximalCombinations(allValidCombos, allPromotions)
 
   /**
-   * Implement a function to find all PromotionCombos for a given Promotion from given list of Promotions.
+   * @param promotionCode the code of the promotion
+   * @param allPromotions all available promotions
+   * @return all PromotionCombos for a given Promotion from given list of Promotions.
    */
-  def combinablePromotions(promotionCode: String, allPromotions: Seq[Promotion]): Seq[PromotionCombo] = ???
+  def combinablePromotions(
+    promotionCode: String,
+    allPromotions: Seq[Promotion]
+  ): Seq[PromotionCombo] =
+    if allPromotions.exists(_.code == promotionCode) then
+      val validCombosWithCode = generateValidCombinationsContaining(promotionCode, allPromotions)
+      findMaximalCombinations(validCombosWithCode, allPromotions)
+    else Seq.empty
+
+  /**
+   * Generate all valid combinations
+   */
+  private def generateAllValidCombinations(allPromotions: Seq[Promotion]): Seq[PromotionCombo] =
+    val codes = allPromotions.map(_.code)
+    val promotionMap = buildPromotionMap(allPromotions)
+
+    for
+      size        <- 1 to codes.length
+      combination <- codes.combinations(size)
+      if isValidCombination(combination, promotionMap)
+    yield PromotionCombo(combination)
+
+  /**
+   * Generate all valid combinations containing the target code
+   */
+  private def generateValidCombinationsContaining(
+    targetCode: String,
+    allPromotions: Seq[Promotion]
+  ): Seq[PromotionCombo] =
+    val otherCodes = allPromotions.map(_.code).filterNot(_ == targetCode)
+    val promotionMap = buildPromotionMap(allPromotions)
+
+    for
+      size        <- 0 to otherCodes.length
+      combination <- otherCodes.combinations(size)
+      fullCombo = targetCode +: combination
+      if isValidCombination(fullCombo, promotionMap)
+    yield PromotionCombo(fullCombo)
+
+  /**
+   * Pick the maximal from valid combinations list.
+   */
+  private def findMaximalCombinations(
+    validCombos: Seq[PromotionCombo],
+    allPromotions: Seq[Promotion]
+  ): Seq[PromotionCombo] =
+    val allCodes = allPromotions.map(_.code)
+    val promotionMap = buildPromotionMap(allPromotions)
+
+    validCombos.filter: combo =>
+      val unusedCodes = allCodes.filterNot(combo.promotionCodes.contains)
+      unusedCodes.forall: newCode =>
+        val extendedCombo = combo.promotionCodes :+ newCode
+        !isValidCombination(extendedCombo, promotionMap)
+
+  /**
+   * Validate whether a given set of promotion codes forms a combinable group.
+   */
+  private def isValidCombination(
+    codes: Seq[String],
+    promotionMap: Map[String, Set[String]]
+  ): Boolean =
+    codes.forall: code =>
+      val incompatibleCodes = promotionMap.getOrElse(code, Set.empty)
+      val otherCodes = codes.filterNot(_ == code)
+      otherCodes.forall(!incompatibleCodes.contains(_))
+
+  /**
+   * Build a lookup map where each promotion code is associated with the list of codes it cannot be combined with. This
+   * precomputation avoids repeatedly scanning the full list of promotions during validation.
+   */
+  private def buildPromotionMap(allPromotions: Seq[Promotion]): Map[String, Set[String]] =
+    allPromotions.groupMapReduce(_.code)(_.notCombinableWith.toSet)(_ ++ _)
---
+++ src/test/scala/dev/gaston/schabas/take/home/zyzygy/problem2/Problem2SolutionTest.scala
@@ -0,0 +1,113 @@
+package dev.gaston.schabas.take.home.zyzygy.problem2
+
+import dev.gaston.schabas.take.home.zyzygy.problem2.Problem2Solution.{Promotion, PromotionCombo}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class Problem2SolutionTest extends AnyFunSuite with Matchers:
+
+  val samplePromotions: Seq[Promotion] = Seq(
+    Promotion("P1", Seq("P3")),
+    Promotion("P2", Seq("P4", "P5")),
+    Promotion("P3", Seq("P1")),
+    Promotion("P4", Seq("P2")),
+    Promotion("P5", Seq("P2"))
+  )
+
+  test("allCombinablePromotions should return all max promotion combos") {
+    val expected = Seq(
+      PromotionCombo(Seq("P1", "P2")),
+      PromotionCombo(Seq("P1", "P4", "P5")),
+      PromotionCombo(Seq("P2", "P3")),
+      PromotionCombo(Seq("P3", "P4", "P5"))
+    )
+
+    val result = Problem2Solution.allCombinablePromotions(samplePromotions)
+    result.map(_.withSortedCodes) should contain theSameElementsAs expected.map(_.withSortedCodes)
+  }
+
+  test("combinablePromotions should return combos containing the given promotion code P1") {
+    val expectedP1 = Seq(
+      PromotionCombo(Seq("P1", "P2")),
+      PromotionCombo(Seq("P1", "P4", "P5"))
+    )
+
+    val resultP1 = Problem2Solution.combinablePromotions("P1", samplePromotions)
+    resultP1.map(_.withSortedCodes) should contain theSameElementsAs expectedP1.map(_.withSortedCodes)
+  }
+
+  test("combinablePromotions should return combos containing the given promotion code P3") {
+    val expectedP3 = Seq(
+      PromotionCombo(Seq("P3", "P2")),
+      PromotionCombo(Seq("P3", "P4", "P5"))
+    )
+
+    val resultP3 = Problem2Solution.combinablePromotions("P3", samplePromotions)
+    resultP3.map(_.withSortedCodes) should contain theSameElementsAs expectedP3.map(_.withSortedCodes)
+  }
+
+  val mutuallyIncompatiblePromotions: Seq[Promotion] = Seq(
+    Promotion("P1", Seq("P2")),
+    Promotion("P2", Seq("P1"))
+  )
+
+  val allIncompatiblePromotions: Seq[Promotion] = Seq(
+    Promotion("P1", Seq("P2", "P3")),
+    Promotion("P2", Seq("P1", "P3")),
+    Promotion("P3", Seq("P1", "P2"))
+  )
+
+  val isolatedPromotions: Seq[Promotion] = Seq(
+    Promotion("P1", Seq("P2", "P3")),
+    Promotion("P2", Seq("P1")),
+    Promotion("P3", Seq("P1"))
+  )
+
+  test("allCombinablePromotions should return individual combos when all are mutually incompatible") {
+    val expected = Seq(
+      PromotionCombo(Seq("P1")),
+      PromotionCombo(Seq("P2"))
+    )
+
+    val result = Problem2Solution.allCombinablePromotions(mutuallyIncompatiblePromotions)
+    result.map(_.withSortedCodes) should contain theSameElementsAs expected.map(_.withSortedCodes)
+  }
+
+  test("allCombinablePromotions should return individual combos when each promotion blocks all others") {
+    val expected = Seq(
+      PromotionCombo(Seq("P1")),
+      PromotionCombo(Seq("P2")),
+      PromotionCombo(Seq("P3"))
+    )
+
+    val result = Problem2Solution.allCombinablePromotions(allIncompatiblePromotions)
+    result.map(_.withSortedCodes) should contain theSameElementsAs expected.map(_.withSortedCodes)
+  }
+
+  test("combinablePromotions should return only individual combo when promotion is isolated") {
+    val expected = Seq(
+      PromotionCombo(Seq("P1"))
+    )
+
+    val result = Problem2Solution.combinablePromotions("P1", isolatedPromotions)
+    result.map(_.withSortedCodes) should contain theSameElementsAs expected.map(_.withSortedCodes)
+  }
+
+  test("combinablePromotions should return combo with others when promotion can combine") {
+    val expected = Seq(
+      PromotionCombo(Seq("P2", "P3"))
+    )
+
+    val result = Problem2Solution.combinablePromotions("P2", isolatedPromotions)
+    result.map(_.withSortedCodes) should contain theSameElementsAs expected.map(_.withSortedCodes)
+  }
+
+  test("allCombinablePromotions should handle empty list") {
+    val result = Problem2Solution.allCombinablePromotions(Seq.empty)
+    result should be(empty)
+  }
+
+  test("combinablePromotions should handle non-existent promotion code") {
+    val result = Problem2Solution.combinablePromotions("P99", samplePromotions)
+    result should be(empty)
+  }
